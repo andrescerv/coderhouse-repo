81d61f39315d4ea331975a9c1c3fe6d7344e8066
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-04 18:15:55 -0600

    agregamos edad

--- script1.py
+++ script1.py
@@ -2,4 +2,5 @@ print('hola mundo!') # 1er cambio en _develop
 
 x = 'Andres' # 3er cambio en _develop
 y = 'Cervantes' # 4to cambio en _develop
-print(f'hola {x}{y}') # 2do cambio en _develop
+edad = 28 # 5to cambio en _develop
+print(f'hola {x}{y}, tienes {edad} años.') # 2do cambio en _develop
