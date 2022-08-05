d3844c8295caf4fe1107f921ca9a7866a169829d
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-04 18:18:08 -0600

    creamo df vacío

--- script1.py
+++ script1.py
@@ -2,3 +2,5 @@ import pandas as pd # 1er cambio en _feature1
 
 print('hola mundo!') # 1er cambio en _develop
 print('hola Andres') # 2do cambio en _develop
+
+df = pd.DataFrame() # 2do cambio en _feature1
