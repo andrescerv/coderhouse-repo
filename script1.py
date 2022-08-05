d50365605c21012780febba6205c7304adeef10d
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-04 18:59:31 -0600

    terminamos empty array con np

--- script1.py
+++ script1.py
@@ -2,3 +2,6 @@ import numpy as np # 1er cambio en _feature2
 
 print('hola mundo!') # 1er cambio en _develop
 print('hola Andres') # 2do cambio en _develop
+
+
+ma_array = np.array() # 2do cambio en _feature2
