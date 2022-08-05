c35d0a37eeff1ea8a83bc7b2860e4df005ca835d
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-04 18:14:55 -0600

    agregamos apellido

--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 print('hola mundo!') # 1er cambio en _develop
 
-x = 'Andres'
-print(f'hola {x}') # 2do cambio en _develop
+x = 'Andres' # 3er cambio en _develop
+y = 'Cervantes' # 4to cambio en _develop
+print(f'hola {x}{y}') # 2do cambio en _develop
