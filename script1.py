f2381e9f0ffc71534bc4a19c34c7efee40a4523f
andrescerv <44825347+andrescerv@users.noreply.github.com>
2022-08-04 19:09:47 -0600

    terminamos con feature 2

--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 import pandas as pd # 1er cambio en _feature1
+import numpy as np
 
 print('hola mundo!') # 1er cambio en _develop
 
@@ -8,3 +9,4 @@ edad = 28 # 5to cambio en _develop
 print(f'hola {nombre} {apellido}, tienes {edad} años.') # 2do cambio en _develop
 
 df = pd.DataFrame() # 2do cambio en _feature1
+ma_arr = np.array()
